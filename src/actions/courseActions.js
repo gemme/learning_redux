d346a13472b1ab0d6797cddfeabb209f73eae55d
gmartinez-ias <gmartinez@itexico.net>
2017-09-05 16:30:33 -0500

    store, mapProps

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,3 +1,3 @@
-export default function courseActions(course) {
+export function createCourse(course) {
     return { type:'CREATE_COURSE', course: course };    
 }
--- src/reducers/courseReducers.js
+++ src/reducers/courseReducers.js
@@ -1,8 +1,8 @@
 export default function courseReducers(state = [], action) {
     switch(action.type){
         case 'CREATE_COURSE':
-            return [ ...state, Object.assign({}, action.course) ]
+            return [ ...state, Object.assign({}, action.course) ];
         default:
          return state;
     }
-};
+}
